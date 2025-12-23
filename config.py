e13be2ff38343e4f1d3eb6197ea6ec17105aa546
krishbharti3404-source <krishbharti3404@gmail.com>
2025-12-23 21:44:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,8 +47,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv("GIT_TOKEN")  # needed if repo is private
 
 # ── Support links ──────────────────────────────────────────────────────────────
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/CertifiedNetwork")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/CertifiedDiscussion")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/iScamz")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/kryshupdate")
 
 # ── Assistant auto-leave ───────────────────────────────────────────────────────
 AUTO_LEAVING_ASSISTANT = False
